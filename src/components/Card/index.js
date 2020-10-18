062ddf1f97815ef53e0f6891f05570874b6b1e3c
Alysson Bortoli <akbortoli@gmail.com>
2020-10-18 23:25:44 +1300

    Remove Card Container

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -13,22 +13,22 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export default function Card() {
   return (
-    <Container>
-      <CardWrapper>
-        <CardHeader>
-          <Icon name="attach-money" size={28} color="#666" />
-          <Icon name="visibility-off" size={28} color="#666" />
-        </CardHeader>
-        <CardBody>
-          <Title>Account Balance</Title>
-          <Description>NZD $16,974.26</Description>
-        </CardBody>
-        <CardFooter>
-          <Annotation>
-            Deposit of NZD $20.00 recived from Daiane Ramos today at 3:00pm
-          </Annotation>
-        </CardFooter>
-      </CardWrapper>
-    </Container>
+    // <Container>
+    <CardWrapper>
+      <CardHeader>
+        <Icon name="attach-money" size={28} color="#666" />
+        <Icon name="visibility-off" size={28} color="#666" />
+      </CardHeader>
+      <CardBody>
+        <Title>Account Balance</Title>
+        <Description>NZD $16,974.26</Description>
+      </CardBody>
+      <CardFooter>
+        <Annotation>
+          Deposit of NZD $20.00 recived from Daiane Ramos today at 3:00pm
+        </Annotation>
+      </CardFooter>
+    </CardWrapper>
+    // </Container>
   );
 }
