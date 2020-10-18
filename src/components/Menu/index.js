804cfe0379bf3dfedb4658ec2f27392be4a03469
Alysson Bortoli <akbortoli@gmail.com>
2020-10-18 23:27:22 +1300

    Replace QRCode with QRCode SVG

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
-import { Container, Code } from './styles';
-import QRCode from 'react-native-qrcode';
+import {
+  Container,
+  Code,
+  Nav,
+  NavItem,
+  NavText,
+  SignOutButton,
+  SignOutButtonText,
+} from './styles';
+import QRCode from 'react-native-qrcode-svg';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export default function Menu() {
@@ -9,17 +17,34 @@ export default function Menu() {
       <Code>
         <QRCode
           value="https://nubank.com.br/"
-          bgColor="#8B10AE"
-          fgColor="#FFF"
+          backgroundColor="#8B10AE"
+          color="#FFF"
           size={80}
         />
       </Code>
 
       <Nav>
         <NavItem>
-          <Icon />
+          <Icon name="help-outline" size={20} color="#FFF" />
+          <NavText>Help</NavText>
+        </NavItem>
+        <NavItem>
+          <Icon name="person-outline" size={20} color="#FFF" />
+          <NavText>Profile</NavText>
+        </NavItem>
+        <NavItem>
+          <Icon name="credit-card" size={20} color="#FFF" />
+          <NavText>Manage Card</NavText>
+        </NavItem>
+        <NavItem>
+          <Icon name="smartphone" size={20} color="#FFF" />
+          <NavText>Settings</NavText>
         </NavItem>
       </Nav>
+
+      <SignOutButton onPress={() => {}}>
+        <SignOutButtonText>Sign Out</SignOutButtonText>
+      </SignOutButton>
     </Container>
   );
 }
