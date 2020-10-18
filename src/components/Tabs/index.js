40564c9c1f698467980986a4bed965d07d6fa17f
Alysson Bortoli <akbortoli@gmail.com>
2020-10-19 00:06:51 +1300

    Adding Pan Gesture to Card Component

--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -2,9 +2,25 @@ import React from 'react';
 import { Container, TabWrapper, TabItem, TabText } from './styles';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-export default function Header() {
+export default function Header({ translateY }) {
   return (
-    <Container>
+    <Container
+      style={{
+        transform: [
+          {
+            translateY: translateY.interpolate({
+              inputRange: [0, 380],
+              outputRange: [0, 30],
+              extrapolate: 'clamp',
+            }),
+          },
+        ],
+        opacity: translateY.interpolate({
+          inputRange: [0, 380],
+          outputRange: [1, 0.3],
+          extrapolate: 'clamp',
+        }),
+      }}>
       <TabWrapper>
         <TabItem>
           <Icon name="person-add" size={24} color="#FFF" />
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,19 +1,89 @@
 import React from 'react';
-import { Container, Content } from './styles';
+
+import { Animated } from 'react-native';
+import { PanGestureHandler, State } from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import {
+  Container,
+  Content,
+  Card,
+  CardHeader,
+  CardContent,
+  CardFooter,
+  Title,
+  Description,
+  Annotation,
+  SafeAreaView,
+} from './styles';
+
 import Header from '~/components/Header';
 import Tabs from '~/components/Tabs';
-import Card from '~/components/Card';
 import Menu from '~/components/Menu';
 
 export default function Main() {
+  let offset = 0;
+  const translateY = new Animated.Value(0);
+  const animatedEvent = new Animated.event(
+    [
+      {
+        nativeEvent: {
+          translationY: translateY,
+        },
+      },
+    ],
+    {
+      useNativeDriver: true,
+    },
+  );
+
+  function onHandlerStateChanged(event) {
+    if (event.nativeEvent.oldState === State.ACTIVE) {
+    }
+  }
+
   return (
-    <Container>
-      <Header />
-      <Content>
-        <Menu />
-        <Card />
-      </Content>
-      <Tabs />
-    </Container>
+    <SafeAreaView>
+      <Container>
+        <Header />
+        <Content>
+          <Menu translateY={translateY} />
+
+          <PanGestureHandler
+            onGestureEvent={animatedEvent}
+            onHandlerStateChange={onHandlerStateChanged}>
+            <Card
+              style={{
+                transform: [
+                  {
+                    translateY: translateY.interpolate({
+                      inputRange: [-350, 0, 380],
+                      outputRange: [-50, 0, 380],
+                      extrapolate: 'clamp',
+                    }),
+                  },
+                ],
+              }}>
+              <CardHeader>
+                <Icon name="attach-money" size={28} color="#666" />
+                <Icon name="visibility-off" size={28} color="#666" />
+              </CardHeader>
+              <CardContent>
+                <Title>Saldo disponível</Title>
+                <Description>R$ 197.611,65</Description>
+              </CardContent>
+              <CardFooter>
+                <Annotation>
+                  Transferência de R$ 20,00 recebida de Diego Schell Fernandes
+                  hoje às 06:00h
+                </Annotation>
+              </CardFooter>
+            </Card>
+          </PanGestureHandler>
+        </Content>
+
+        <Tabs translateY={translateY} />
+      </Container>
+    </SafeAreaView>
   );
 }
