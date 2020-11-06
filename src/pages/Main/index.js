950c66403d39665e365049317ace4667f5ed75a6
Alysson Bortoli <akbortoli@gmail.com>
2020-11-06 14:06:43 +1300

    Finish Card Animation

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -39,6 +39,29 @@ export default function Main() {
 
   function onHandlerStateChanged(event) {
     if (event.nativeEvent.oldState === State.ACTIVE) {
+      let opened = false;
+      const { translationY } = event.nativeEvent;
+
+      offset += translationY;
+
+      if (translationY >= 100) {
+        opened = true;
+      } else {
+        translateY.setValue(offset);
+        translateY.setOffset(0);
+        offset = 0;
+      }
+
+      Animated.timing(translationY, {
+        toValue: opened ? 380 : 0,
+        duration: 200,
+        useNativeDriver: true,
+      }).start(() => {
+        offset = opened ? 380 : 0;
+
+        translateY.setOffset(offset);
+        translateY.setValue(0);
+      });
     }
   }
 
